8c9ace3818bf044e349dc325b8c20bbb70e889c1
murtuzzov <murtuzzov>
2024-03-15 23:26:36 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// ЗАДАЧА No1 | создайте объект person с полями name, age, city и country.
+//Используйте деструктуризацию объекта для извлечения значений
+//name и age в отдельные переменные.
+
+// const person = {
+//   username: "Murtuz",
+//   age: 19,
+//   city: "Baku",
+//   country: "Azerbaijan",
+// };
+
+// const { username, age } = person;
+
+// console.log(username);
+// console.log(age);
